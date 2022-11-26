ac156a98eb52782a82040df1e0f62f18f52b6053
Pham Van Khang <phamvankhang.tvi@gmail.com>
2022-11-27 00:30:28 +0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+    return (
+        <div style={{ textAlign: "center" }}>
+            <h1>Hello Khang dev!</h1>
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
